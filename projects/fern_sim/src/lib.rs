96277e913b79fc133a00178989cff05371e6cf8b
keisuke-umezawa <keisuke.umezawa@gmail.com>
2019-06-16 22:40:40 +0900

    Add

---
+++ projects/fern_sim/src/lib.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub struct Fern {
+    pub size: f64,
+    pub growth_rate: f64
+}
+
+impl Fern {
+    /// Simulate a fern growing for one day.
+    pub fn grow(&mut self) {
+        self.size *= 1.0 + self.growth_rate;
+    }
+}
+
+/// Run a fern simulation for some number of days.
+pub fn run_simulation(fern: &mut Fern, days: usize) {
+    for _ in 0 .. days {
+        fern.grow();
+    }
+}
---
+++ projects/practicea/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+
+fn main() {
+    let patterns: Vec<Vec<char>> = ["dream", "dreamer", "erase", "eraser"]
+        .iter()
+        .map(|s| s.chars().rev().collect())
+        .collect();
+    let s: Vec<char> = read::<String>().chars().rev().collect();
+
+    let mut s = &s[..];
+    let mut succeeded = true;
+
+    while s.len() > 0 {
+        match patterns.iter().find(|&p| s.starts_with(p)) {
+            Some(x) => {
+                s = &s[x.len()..];
+            }
+            _ => {
+                succeeded = false;
+                break;
+            }
+        }
+    }
+    println!("{}", if succeeded { "YES" } else { "NO" });
+}
